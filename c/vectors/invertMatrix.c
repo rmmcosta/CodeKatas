c4da6f0bf85f9f21a3335d9a19828d0808f60271
rmmcosta <ricardocosta101085@gmail.com>
2021-03-15 22:06:19 +0000

    operations with matrixes

--- c/vectors/invertMatrix.c
+++ c/vectors/invertMatrix.c
@@ -2,10 +2,12 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define MAX 5
+#define MAX 3
 #define DEFAULT_SEPARATOR_SIZE 10
+#define MAX_VALUE 9
 
 void invert(int v[MAX][MAX]);
+void invert2(int v[MAX][MAX]);
 void printMatrix(int v[MAX][MAX]);
 void fillMatrix(int v[MAX][MAX]);
 void printSeparator(int n);
@@ -19,11 +21,40 @@ int main()
     invert(matrix);
     printSeparator(DEFAULT_SEPARATOR_SIZE);
     printMatrix(matrix);
+    invert2(matrix);
+    printSeparator(DEFAULT_SEPARATOR_SIZE);
+    printMatrix(matrix);
     return 0;
 }
 
 void invert(int v[MAX][MAX])
 {
+    int temp;
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            if (i == MAX - i - 1 && j == MAX - j - 1)
+                return;
+            temp = v[i][j];
+            v[i][j] = v[MAX - i - 1][MAX - j - 1];
+            v[MAX - i - 1][MAX - j - 1] = temp;
+        }
+    }
+}
+
+void invert2(int v[MAX][MAX])
+{
+    int temp;
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = i + 1; j < MAX; j++)
+        {
+            temp = v[i][j];
+            v[i][j] = v[j][i];
+            v[j][i] = temp;
+        }
+    }
 }
 
 void printMatrix(int v[MAX][MAX])
@@ -43,7 +74,7 @@ void fillMatrix(int v[MAX][MAX])
     srand(ultime);
     for (int i = 0; i < MAX; i++)
         for (int j = 0; j < MAX; j++)
-            v[i][j] = rand() % 99 + 1;
+            v[i][j] = rand() % MAX_VALUE + 1;
 }
 
 void printSeparator(int n)
